1846e0111cf2fc3459d97511196e1c6664d8bef3
mohamedkamal274 <muhammedkmal3@gmail.com>
2016-11-01 23:31:14 +0200

    Function columnChecker and rowChecker
    Function columnChecker and rowChecker add to there class and tested

--- src/sudoku/rowChecker.java
+++ src/sudoku/rowChecker.java
@@ -5,11 +5,16 @@
  */
 package sudoku;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  *
  * @author mohamedkamal
  */
-public class rowChecker extends sudoku implements Runnable{
+public class rowChecker extends sudoku implements Runnable {
 
     @Override
     public void run() {
@@ -17,7 +22,20 @@ public class rowChecker extends sudoku implements Runnable{
     }
 
     private void checkrow() {
-        //TODO
+        Set<Integer> myset = new HashSet<>();
+        for (int row = 0; row < this.sudoku.length; row++) {
+            Set<Integer> errorSet = new HashSet<>();
+            for (int column = 0; column < this.sudoku.length; column++) {
+                if (!myset.add(this.sudoku[row][column])) {
+                    errorSet.add(Arrays.asList(this.sudoku[row]).indexOf(this.sudoku[row][column]));
+                    errorSet.add(column);
+                }
+            }
+            for (Iterator<Integer> setValues = errorSet.iterator(); setValues.hasNext();) {
+                this.sudokuError[row][setValues.next()] = Boolean.TRUE;
+            }
+            myset.clear();
+        }
     }
-    
+
 }
