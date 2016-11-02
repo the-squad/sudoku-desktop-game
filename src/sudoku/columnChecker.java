833f33e6dc7adadbd4447222d99042160b069981
Muhammad Tarek <muhammadtarek96@gmail.com>
2016-11-02 13:33:58 +0200

    Formatting code

--- src/sudoku/columnChecker.java
+++ src/sudoku/columnChecker.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sudoku;
 
 import java.util.ArrayList;
@@ -12,10 +7,6 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
-/**
- *
- * @author mohamedkamal
- */
 public class columnChecker extends sudoku implements Runnable {
 
     @Override
@@ -47,5 +38,4 @@ public class columnChecker extends sudoku implements Runnable {
         }
         return colArray;
     }
-
 }
