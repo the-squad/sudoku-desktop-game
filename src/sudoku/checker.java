9f700f04fe4b47b6eed10e726d17d3d9f26de11b
Muhammad Tarek <muhammadtarek96@gmail.com>
2016-11-02 13:28:11 +0200

    Formatting code

--- src/sudoku/checker.java
+++ src/sudoku/checker.java
@@ -1,27 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sudoku;
 
-/**
- *
- * @author mohamedkamal
- */
 public class checker {
+    
+    //Creating objects from checker classes
     boxChecker box;
     rowChecker row;
     columnChecker column;
 
     public checker() {
+        //Init objects
         this.box = new boxChecker();
         this.row = new rowChecker();
         this.column = new columnChecker();
     }
     
-    void check(sudoku su)
-    {
+    void check(sudoku su {
         
     }
 }
