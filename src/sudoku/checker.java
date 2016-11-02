60135348dfeebf80a4f54d0a142186aba8f4682b
mohamedkamal274 <muhammedkmal3@gmail.com>
2016-11-03 00:20:28 +0200

    boxChecker
    boxchecker added and check class

--- src/sudoku/checker.java
+++ src/sudoku/checker.java
@@ -1,20 +1,31 @@
 package sudoku;
 
 public class checker {
-    
+
     //Creating objects from checker classes
     boxChecker box;
     rowChecker row;
     columnChecker column;
 
-    public checker() {
-        //Init objects
-        this.box = new boxChecker();
+    /*public checker() {
+     //Init objects
+     this.box = new boxChecker();
+     this.row = new rowChecker();
+     this.column = new columnChecker();
+     }*/
+    void check() {
         this.row = new rowChecker();
         this.column = new columnChecker();
-    }
-    
-    void check(sudoku su) {
-        
+        Thread thread;
+        thread = new Thread(this.row);
+        thread = new Thread(this.column);
+        int threadId = 0;
+        for (int row = 0; row < 9; row += 3) {
+            for (int col = 0; col < 9; col += 3) {
+                this.box = new boxChecker(row, col, threadId);
+                thread = new Thread(this.box);
+                threadId++;
+            }
+        }
     }
 }
