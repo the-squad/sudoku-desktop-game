b2004eee9f27d9b9226f5e24fbb8a6b8ca874233
Muhammad Tarek <muhammadtarek96@gmail.com>
2016-11-02 13:23:50 +0200

    Formatting code and renaming variables

--- src/sudoku/sudoku.java
+++ src/sudoku/sudoku.java
@@ -1,28 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sudoku;
 
 import java.util.Arrays;
 
-/**
- *
- * @author mohamedkamal
- */
 public class sudoku {
     protected static Integer[][] sudoku;
-    protected static Boolean[][] sudokuError;
-    public void initsudokuError(){
-        sudokuError = new Boolean[9][9];
-        for(Boolean row[] : sudokuError)
-        {
+    protected static Boolean[][] sudokuWrongCells;
+    
+    public void initSudokuWrongCells() {
+        sudokuWrongCells = new Boolean[9][9];
+        
+        for(Boolean row[] : sudokuWrongCells) {
             Arrays.fill(row, Boolean.FALSE);
         }
     }
-    public Boolean[][] getSudokuError() {
-        return sudokuError;
+    
+    public Boolean[][] getsudokuWrongCells() {
+        return sudokuWrongCells;
     }
     
     public Integer[][] getSudoku() {
@@ -33,8 +26,12 @@ public class sudoku {
         this.sudoku = sudoku;
     }
     
-    public Integer[][] solveSudoku(){
+    public Integer[][] solveSudoku() {
         //TODO
+        
+        /*
+            This method will solve the Sudoku user have enetered
+        */
         return null;
     }
 }
