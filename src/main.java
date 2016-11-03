ae8194caae5f84aad2f6399ba65143c3ca231a99
Muhammad Tarek <muhammadtarek96@gmail.com>
2016-11-03 20:09:26 +0200

    Adding white card

--- src/main.java
+++ src/main.java
@@ -6,10 +6,13 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 
 public class main extends Application {
 
@@ -18,10 +21,12 @@ public class main extends Application {
     Integer[][] userSudoku; //Reads the Sudoku from the user
     Integer[][] computerSolution; //Where computer returns the wrong cells
     
-    TextField sudokuCells[][]; //FIXME
+    TextField[][] sudokuCells; //FIXME
 
     Scene mainMenuScene; //Where the user will start the app
     Scene gameScene; //Where the user will enter or play Sudoku
+    
+    Label cardBg;
 
     /*
      Playing mode controls what happens and shows on the gameScene
@@ -57,6 +62,9 @@ public class main extends Application {
         //Creating page headline
         Label headline = new Label("Check your Sudoku");
         headline.setId("headline");
+        
+        headline.setMaxWidth(Double.MAX_VALUE);
+        headline.setAlignment(Pos.CENTER);
 
         //Creating control buttons
         Button back = new Button("");
@@ -68,12 +76,17 @@ public class main extends Application {
         save.setId("saveButton"); //Setting ID
 
         //Setting position
-        toolbarLayout.setCenter(headline);
+        toolbarLayout.setTop(headline);
         toolbarLayout.setLeft(back);
         toolbarLayout.setRight(save);
+        
+        initSudokuBlock();
 
         //Adding everything into the layout
         toolbarLayout.getChildren().addAll();
+        
+        
+        
 
         //Adding the toolbar in the top of the window
         gameSceneLayout.setTop(toolbarLayout);
@@ -84,7 +97,10 @@ public class main extends Application {
     }
 
     private void initSudokuBlock() {
-        sudokuCells = new TextFields();
+        
+        cardBg = new Label();
+        cardBg.getStyleClass().add("card");
+
         int rowCounter, columnCounter;
         
         //Creating Sudoku cells
