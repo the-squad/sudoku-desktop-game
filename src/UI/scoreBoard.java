fbe7677e2e546113917a3a69ad1a89824d8f1548
Muhammad Tarek <muhammadtarek96@gmail.com>
2016-11-18 22:39:29 +0200

    Score Board is finished

--- src/UI/scoreBoard.java
+++ src/UI/scoreBoard.java
@@ -1,17 +1,27 @@
 package UI;
 
+import static UI.global.FADE_OUT;
+import static UI.global.fade;
 import static UI.global.windowLayout;
 import static UI.global.mainMenuContainer;
 import static UI.global.switchPanes;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.event.EventHandler;
 
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.Point2D;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
+import javafx.util.Duration;
 
 /**
  *
@@ -20,6 +30,19 @@ import javafx.scene.layout.GridPane;
 public class scoreBoard {
 
     GridPane scorePageContainer;
+    private GridPane saveDataLayout;
+    private Label inputLabel;
+    private TextField nameTextField;
+    private Label scoreBoardArray[] = new Label[5];
+    private Label timeLabel;
+
+    public void setScoreBoardArray(Label[] scoreBoardArray) {
+        this.scoreBoardArray = scoreBoardArray;
+    }
+
+    public void setTimeLabel(Label timeLabel) {
+        this.timeLabel = timeLabel;
+    }
 
     public GridPane initialize() {
         //Score page layout
@@ -27,65 +50,83 @@ public class scoreBoard {
         scorePageContainer.getStyleClass().add("score-page");
         scorePageContainer.setAlignment(Pos.CENTER);
 
+        //Page Headline
         Label headline = new Label("Time");
         headline.getStyleClass().add("score-text");
         headline.getStyleClass().add("page-headline");
-        headline.setPadding(new Insets(0 ,0 ,5, 0));
+        headline.setPadding(new Insets(0, 0, 5, 0));
         centerObject(headline);
         scorePageContainer.setConstraints(headline, 0, 0);
 
-        Label timeLabel = new Label("1:00");
+        //Game time
+        timeLabel = new Label("1:00");
         timeLabel.getStyleClass().add("score-text");
         timeLabel.getStyleClass().add("game-score-time");
         centerObject(timeLabel);
         scorePageContainer.setConstraints(timeLabel, 0, 1);
 
-        GridPane saveDataLayout = new GridPane();
+        //Left panel layout
+        saveDataLayout = new GridPane();
         centerObject(saveDataLayout);
         scorePageContainer.setConstraints(saveDataLayout, 0, 2);
 
-        Label inputLabel = new Label("Enter you name:");
+        //Textfield placeholder
+        inputLabel = new Label("Enter you name:");
         inputLabel.getStyleClass().add("input-label");
         inputLabel.getStyleClass().add("score-text");
         saveDataLayout.setConstraints(inputLabel, 0, 0);
-        
-        TextField nameTextField = new TextField();
+
+        //Textfield
+        nameTextField = new TextField();
         nameTextField.getStyleClass().add("name-text-field");
         saveDataLayout.setConstraints(nameTextField, 1, 0);
-        
-        Button nameSumbitButton = new Button("SAVE");
-        nameSumbitButton.getStyleClass().add("score-button");
-        nameSumbitButton.setPadding(new Insets(5, 20, 5, 20));
-        nameSumbitButton.setTranslateX(20);
-        saveDataLayout.setConstraints(nameSumbitButton, 2, 0);
 
-        saveDataLayout.getChildren().addAll(inputLabel, nameTextField, nameSumbitButton);
+        nameTextField.setOnKeyPressed((KeyEvent keyEvent) -> {
+            if (keyEvent.getCode() == KeyCode.ENTER) {
+                fade(inputLabel, 300, 0, FADE_OUT);
+                Timeline changeTextfieldTimeline = new Timeline();
+                
+                KeyValue startDisable = new KeyValue(nameTextField.disableProperty(), false);
+                KeyValue endDisable = new KeyValue(nameTextField.disableProperty(), true);
+                
+                KeyFrame startAction = new KeyFrame(Duration.ZERO, startDisable);
+                KeyFrame endAction = new KeyFrame(Duration.millis(250), endDisable);
+                KeyFrame removeLabel = new KeyFrame(Duration.millis(300), e -> {
+                    saveDataLayout.getChildren().remove(inputLabel);
+                    saveDataLayout.setAlignment(Pos.CENTER);
+                    nameTextField.setAlignment(Pos.CENTER);
+                });
+                changeTextfieldTimeline.getKeyFrames().addAll(startAction, endAction, removeLabel);
+                changeTextfieldTimeline.play();
+            }
+        });
+
+        saveDataLayout.getChildren().addAll(inputLabel, nameTextField);
 
         Label boardHeadline = new Label("Time Score Board");
         boardHeadline.getStyleClass().add("score-text");
         boardHeadline.getStyleClass().add("page-headline");
-        boardHeadline.setPadding(new Insets(80 ,0 ,15, 0));
+        boardHeadline.setPadding(new Insets(50, 0, 15, 0));
         scorePageContainer.setConstraints(boardHeadline, 0, 3);
         centerObject(boardHeadline);
 
         scorePageContainer.getChildren().addAll(headline, timeLabel, saveDataLayout, boardHeadline);
-
-        Label scoreBoardArray[] = new Label[5];
 
         for (int counter = 0; counter < 5; counter++) {
             scoreBoardArray[counter] = new Label(counter + 1 + ".        1:00        Muhammad Tarek");
             scoreBoardArray[counter].getStyleClass().add("score-line");
             scoreBoardArray[counter].getStyleClass().add("score-text");
             centerObject(scoreBoardArray[counter]);
-            
-            if (counter < 4)
+
+            if (counter < 4) {
                 scoreBoardArray[counter].setPadding(new Insets(5, 0, 5, 0));
-            else
+            } else {
                 scoreBoardArray[counter].setPadding(new Insets(5, 0, 30, 0));
-            
+            }
+
             scorePageContainer.setConstraints(scoreBoardArray[counter], 0, 4 + counter + 1);
             scorePageContainer.getChildren().add(scoreBoardArray[counter]);
-        } 
+        }
 
         Button backToMenuButton = new Button("BACK TO MAIN MENU");
         backToMenuButton.getStyleClass().add("score-button");
@@ -94,10 +135,30 @@ public class scoreBoard {
         scorePageContainer.setConstraints(backToMenuButton, 0, 10);
         centerObject(backToMenuButton);
         scorePageContainer.getChildren().add(backToMenuButton);
-        
-        backToMenuButton.setOnAction(e -> switchPanes(windowLayout, scorePageContainer, mainMenuContainer));
+
+        backToMenuButton.setOnAction(e -> {
+            switchPanes(windowLayout, scorePageContainer, mainMenuContainer);
+            Timeline resetUITimeline = new Timeline();
+            KeyFrame startReset = new KeyFrame(Duration.millis(300), event -> resetUI());
+            resetUITimeline.getKeyFrames().add(startReset);
+            resetUITimeline.play();
+            
+        });
 
         return scorePageContainer;
+    }
+
+    static void printTimeRecords() {
+        //Edit records text
+    }
+
+    static void printGameTime() {
+        //Edit game time
+    }
+
+    private void resetUI() {
+        saveDataLayout.setConstraints(inputLabel, 0, 0);
+        nameTextField.setDisable(false);
     }
 
     private void centerObject(Object node) {
