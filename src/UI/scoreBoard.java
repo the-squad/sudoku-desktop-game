32885a4401eba17de2f506987b44d8460b3b3839
Muhammad Tarek <muhammadtarek96@gmail.com>
2016-11-18 19:59:38 +0200

    Finished score page
    NEXT: adding method to control text in the score page and buttons
    actions

--- src/UI/scoreBoard.java
+++ src/UI/scoreBoard.java
@@ -5,6 +5,7 @@ import static UI.global.mainMenuContainer;
 import static UI.global.switchPanes;
 
 import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
@@ -29,6 +30,7 @@ public class scoreBoard {
         Label headline = new Label("Time");
         headline.getStyleClass().add("score-text");
         headline.getStyleClass().add("page-headline");
+        headline.setPadding(new Insets(0 ,0 ,5, 0));
         centerObject(headline);
         scorePageContainer.setConstraints(headline, 0, 0);
 
@@ -46,11 +48,15 @@ public class scoreBoard {
         inputLabel.getStyleClass().add("input-label");
         inputLabel.getStyleClass().add("score-text");
         saveDataLayout.setConstraints(inputLabel, 0, 0);
+        
         TextField nameTextField = new TextField();
         nameTextField.getStyleClass().add("name-text-field");
         saveDataLayout.setConstraints(nameTextField, 1, 0);
-        Button nameSumbitButton = new Button("Save");
+        
+        Button nameSumbitButton = new Button("SAVE");
         nameSumbitButton.getStyleClass().add("score-button");
+        nameSumbitButton.setPadding(new Insets(5, 20, 5, 20));
+        nameSumbitButton.setTranslateX(20);
         saveDataLayout.setConstraints(nameSumbitButton, 2, 0);
 
         saveDataLayout.getChildren().addAll(inputLabel, nameTextField, nameSumbitButton);
@@ -58,6 +64,7 @@ public class scoreBoard {
         Label boardHeadline = new Label("Time Score Board");
         boardHeadline.getStyleClass().add("score-text");
         boardHeadline.getStyleClass().add("page-headline");
+        boardHeadline.setPadding(new Insets(80 ,0 ,15, 0));
         scorePageContainer.setConstraints(boardHeadline, 0, 3);
         centerObject(boardHeadline);
 
@@ -66,17 +73,24 @@ public class scoreBoard {
         Label scoreBoardArray[] = new Label[5];
 
         for (int counter = 0; counter < 5; counter++) {
-            scoreBoardArray[counter] = new Label("1.        1:00        Muhammad Tarek");
+            scoreBoardArray[counter] = new Label(counter + 1 + ".        1:00        Muhammad Tarek");
             scoreBoardArray[counter].getStyleClass().add("score-line");
             scoreBoardArray[counter].getStyleClass().add("score-text");
             centerObject(scoreBoardArray[counter]);
+            
+            if (counter < 4)
+                scoreBoardArray[counter].setPadding(new Insets(5, 0, 5, 0));
+            else
+                scoreBoardArray[counter].setPadding(new Insets(5, 0, 30, 0));
             
             scorePageContainer.setConstraints(scoreBoardArray[counter], 0, 4 + counter + 1);
             scorePageContainer.getChildren().add(scoreBoardArray[counter]);
         } 
 
-        Button backToMenuButton = new Button("Back to Main Menu");
+        Button backToMenuButton = new Button("BACK TO MAIN MENU");
         backToMenuButton.getStyleClass().add("score-button");
+        backToMenuButton.setId("back--button");
+        backToMenuButton.setPadding(new Insets(10, 20, 10, 20));
         scorePageContainer.setConstraints(backToMenuButton, 0, 10);
         centerObject(backToMenuButton);
         scorePageContainer.getChildren().add(backToMenuButton);
